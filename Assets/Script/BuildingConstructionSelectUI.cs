ff49aadcf08bc6c51c1d9a68a15610b29c47af37
Noah Bathras <nbathras@terpmail.umd.edu>
2020-09-27 14:07:02 -0400

    Added building UI Queue structure with debugging functions

--- Assets/Script/BuildingConstructionSelectUI.cs
+++ Assets/Script/BuildingConstructionSelectUI.cs
@@ -4,24 +4,62 @@ using UnityEngine.UI;
 
 public class BuildingConstructionSelectUI : MonoBehaviour {
 
+    // Test fields
+    [SerializeField] private UnitTypeSO testUnitType;
+
+    // Serialized
+    [SerializeField] private Vector2 initialOffSet = new Vector2(75f, 75f);
+    // [SerializeField] private Vector2 initialOffsetQueue = new Vector2(-75f, 75f);
+    [SerializeField] private Vector2 offset = new Vector2(125f, 0f);
+    // [SerializeField] private Vector2 offsetQueue = new Vector2(-125f, 0f);
+
+    // Set in Awake
+    private Transform optionContainer;
+    private Transform btnTemplateOption;
+    private Transform queueContainer;
+    private Transform btnTemplateQueue;
+
+    // Other
     private Dictionary<UnitTypeSO, Transform> btnTransformDictionary;
+    private Queue<ConstructionQueueEntry> constructionQueue; 
 
     private void Awake() {
-        Transform btnTemplate = transform.Find("btnTemplate");
-        btnTemplate.gameObject.SetActive(false);
+        // Get btn templates
+        optionContainer = transform.Find("optionContainer");
+        btnTemplateOption = optionContainer.Find("btnTemplateOption");
+        btnTemplateOption.gameObject.SetActive(false);
 
-        UnitTypeListSO unitTypeList = Resources.Load<UnitTypeListSO>(typeof(UnitTypeListSO).Name);
+        queueContainer = transform.Find("queueContainer");
+        btnTemplateQueue = queueContainer.Find("btnTemplateQueue");
+        btnTemplateQueue.gameObject.SetActive(false);
 
+        // Initalize Data Structures
         btnTransformDictionary = new Dictionary<UnitTypeSO, Transform>();
 
+        constructionQueue = new Queue<ConstructionQueueEntry>();
+
+        UpdateOptions();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            AddConstructionQueueEntry();
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            PopConstructionQueueEntry();
+        }
+    }
+
+    private void UpdateOptions() {
+        UnitTypeListSO unitTypeList = Resources.Load<UnitTypeListSO>(typeof(UnitTypeListSO).Name);
         int index = 0;
-        float offsetAmountInital = 75f;
-        float offsetAmount = 125f;
         foreach (UnitTypeSO unitType in unitTypeList.list) {
-            Transform btnTransform = Instantiate(btnTemplate, transform);
+            Transform btnTransform = Instantiate(btnTemplateOption, transform);
+            btnTransform.SetParent(optionContainer);
             btnTransform.gameObject.SetActive(true);
 
-            btnTransform.GetComponent<RectTransform>().anchoredPosition = new Vector3(offsetAmountInital + offsetAmount * index, 75);
+            btnTransform.GetComponent<RectTransform>().anchoredPosition = initialOffSet + offset * index;
 
             btnTransform.Find("text").GetComponent<Text>().text = unitType.name;
 
@@ -32,6 +70,53 @@ public class BuildingConstructionSelectUI : MonoBehaviour {
             btnTransformDictionary[unitType] = btnTransform;
 
             index++;
+        }
+    }
+
+    private bool AddConstructionQueueEntry() {
+        if (constructionQueue.Count >= 5) {
+            return false;
+        }
+        Transform btnTransform = Instantiate(btnTemplateQueue, transform);
+        btnTransform.SetParent(queueContainer);
+        btnTransform.gameObject.SetActive(true);
+
+        btnTransform.GetComponent<RectTransform>().anchoredPosition = initialOffSet + offset * constructionQueue.Count;
+
+        btnTransform.Find("text").GetComponent<Text>().text = testUnitType.name + constructionQueue.Count;
+
+        btnTransform.GetComponent<Button>().onClick.AddListener(() => {
+            // Click here
+        });
+
+        constructionQueue.Enqueue(new ConstructionQueueEntry(btnTransform, testUnitType));
+
+        return true;
+    }
+
+    private UnitTypeSO PopConstructionQueueEntry() {
+        if (constructionQueue.Count == 0) {
+            return null;
+        } 
+        ConstructionQueueEntry constructionQueueEntry = constructionQueue.Dequeue();
+        Destroy(constructionQueueEntry.btn.gameObject);
+
+        int index = 0;
+        foreach (ConstructionQueueEntry entry in constructionQueue) {
+            entry.btn.GetComponent<RectTransform>().anchoredPosition = initialOffSet + offset * index;
+            index++;
+        }
+
+        return constructionQueueEntry.unitType;
+    }
+
+    private class ConstructionQueueEntry {
+        public Transform btn;
+        public UnitTypeSO unitType;
+
+        public ConstructionQueueEntry(Transform inBtn, UnitTypeSO inUnitType) {
+            btn = inBtn;
+            unitType = inUnitType;
         }
     }
 }
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField] private int targetFrameRate = 60;
+
+    void Awake() {
+        Application.targetFrameRate = targetFrameRate;
+    }
+}
